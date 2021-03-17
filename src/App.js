d127be604104d57cac2ae7c313b1b2c7db564cc4
AnmolDixit <ADixit3@ad.harman.com>
2021-03-17 21:09:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {todos: []};
+  }
+
+  toggleTodo(todoId) {
+    const todos = this.state.todos;
+    for (const t of todos) {
+      if (t.id === todoId) t.completed = !t.completed;
+    }
+    this.setState({ todos: todos });
+  }
+
+  deleteTodo(todoId) {
+    const todos = [];
+    for (const t of this.state.todos){
+      if(t.id !== todoId){
+        todos.push(t);
+      }
+    }
+    this.setState({ todos });
+  }
+
+  renderToDo(todo) {
+    return (
+      <>
+        <div>{todo.task}</div>
+        <span>Is to do completed? - {todo.completed.toString()}</span>
+        <button onClick={() => this.toggleTodo(todo.id)}>Check</button>
+        <button onClick={() => this.deleteTodo(todo.id)}>Delete</button>
+      </>
+    );
+  }
+
+  renderToDoList() {
+    return (
+      <>
+      <div>
+        {this.state.todos.map(todo => this.renderToDo(todo))}
+      </div>
+      </>
+    )
+  }
+
+  getNumberOfPendingTasks () {
+    let counter=0;
+    for (const t1 of this.state.todos) {
+      if(t1.completed === false) counter++;
+    }
+    return counter;
+  }
+
+  addNewTask() {
+    const todos = this.state.todos;
+    const newToDo = {
+      task: "",
+      completed: false,
+      id: nanoid()
+    };
+    newToDo.task = `Task ${this.state.todos.length + 1}`;
+    console.log('New to do is: ', newToDo);
+    todos.push(newToDo);
+    this.setState({ todos });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>To Do App</h1>
+        <h2>Total Number of Tasks: {this.state.todos.length}</h2>
+        <h2>Pending Tasks: {this.getNumberOfPendingTasks()}</h2>
+        {this.renderToDoList()}
+        <button onClick={() => this.addNewTask()}>Add New Task</button>
+      </div>
+    );
+  }
 }
 
 export default App;
